c3aac8a3dfabcfc11f724abcd84d5a9a57b9ee84
DheerajKukreja9554 <81379140+DheerajKukreja9554@users.noreply.github.com>
2022-03-31 16:02:10 +0530

    updated jar 4

--- src/main/java/com/liberin/test/controller/MyController.java
+++ src/main/java/com/liberin/test/controller/MyController.java
@@ -4,14 +4,14 @@ import java.util.Map;
 
 import com.liberin.icoreparser.controller.ParseController;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
     
-    @GetMapping("/mt940")
+    @PostMapping("/mt940")
     public Map<String,Object> toHashMap(@RequestBody String text) {
         
         ParseController controller = new ParseController();
