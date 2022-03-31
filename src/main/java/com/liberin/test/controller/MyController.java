2a7a7d0efa737f31a21a02c263907937412368ac
DheerajKukreja9554 <81379140+DheerajKukreja9554@users.noreply.github.com>
2022-03-31 16:16:20 +0530

    update jar 5

--- src/main/java/com/liberin/test/controller/MyController.java
+++ src/main/java/com/liberin/test/controller/MyController.java
@@ -1,9 +1,11 @@
 package com.liberin.test.controller;
 
+import java.io.FileWriter;
 import java.util.Map;
 
 import com.liberin.icoreparser.controller.ParseController;
 
+import org.apache.logging.log4j.message.Message;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,13 @@ public class MyController {
     public Map<String,Object> toHashMap(@RequestBody String text) {
         
         ParseController controller = new ParseController();
-        return controller.convertToMT940(text);
+        Map<String,Object> answer = controller.convertToMT940(text);
+
+        try (FileWriter writer = new FileWriter("./"+answer.get("file name"))) {
+            writer.write(answer.get("MT940 message").toString());
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        return answer;
     }
 }
