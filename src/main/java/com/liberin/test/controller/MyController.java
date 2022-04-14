1d1b034465691bf0a840597fe483fc86beb5ffbf
DheerajKukreja9554 <81379140+DheerajKukreja9554@users.noreply.github.com>
2022-04-14 12:14:29 +0530

    updated jar 6

--- src/main/java/com/liberin/test/controller/MyController.java
+++ src/main/java/com/liberin/test/controller/MyController.java
@@ -3,9 +3,10 @@ package com.liberin.test.controller;
 import java.io.FileWriter;
 import java.util.Map;
 
-import com.liberin.icoreparser.controller.ParseController;
+import com.fci.icoreparser.MT940Converter;
 
-import org.apache.logging.log4j.message.Message;
+// import com.fci.icoreparser.controller.ParseController;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +17,11 @@ public class MyController {
     @PostMapping("/mt940")
     public Map<String,Object> toHashMap(@RequestBody String text) {
         
-        ParseController controller = new ParseController();
-        Map<String,Object> answer = controller.convertToMT940(text);
+        
+        Map<String,Object> answer = MT940Converter.convertToMT940(text);
 
         try (FileWriter writer = new FileWriter("./"+answer.get("file name"))) {
+            System.out.println("writing");
             writer.write(answer.get("MT940 message").toString());
         } catch (Exception e) {
             //TODO: handle exception
